6d73a1115af3b334bd05098c8c75f4627d4933fe
Matheus Castello <matheus@castello.eng.br>
2023-11-06 08:35:20 -0300

    linuxKernelDevIssues: Add logic to not check the same issues again
    Signed-off-by: Matheus Castello <matheus@castello.eng.br>

--- linuxKernelDevIssues.ps1
+++ linuxKernelDevIssues.ps1
@@ -3,6 +3,24 @@
 )]
 param()
 
+if ($null -eq ("Issue" -as [type])) {
+Add-Type -TypeDefinition @"
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Issue {
+    public int id { get; set; }
+}
+
+public class Issues : ArrayList {
+    public Issues() : base() {}
+}
+"@
+}
+
+$CHECKED = "$PSScriptRoot/../versions/linuxkerneldev"
+
 # ret object
 $ret = [PSCustomObject]@{
     lines = [System.Collections.ArrayList]::new()
@@ -18,14 +36,56 @@ $obj = ConvertFrom-Json $get
 # how many issues we have
 $count = $obj.Count
 
-if ($count -gt 0) {
+# check if we have something from ../versions/linuxkerneldev
+# check if the file exists
+if (-not (Test-Path $CHECKED)) {
+    # creat it
+    # obj to json
+    $json = ConvertTo-Json -InputObject (New-Object Issues)
+
+    # save it
+    $json | Out-File "$PSScriptRoot/../versions/linuxkerneldev"
+}
+
+# read it
+$json = Get-Content "$PSScriptRoot/../versions/linuxkerneldev" | ConvertFrom-Json
+$notCheckedIssues = [System.Collections.ArrayList]::new()
+$issues = $json
+
+if ($null -eq $issues) {
+    $issues = New-Object Issues
+}
+
+# check if we have new issues
+foreach ($ghIssue in $obj) {
+    # check if the id exists in $issues
+    $id = $ghIssue.id
+
+    $idExists = $issues | Where-Object { $_.id -eq $id }
+
+    if (-not $idExists) {
+        $notCheckedIssues.Add($ghIssue)
+        $issues.Add([Issue]@{
+            id = $id
+        })
+    }
+}
+
+# ok, we need to save now the checked issues
+# obj to json
+$json = ConvertTo-Json -InputObject $issues
+
+# save it
+$json | Out-File "$PSScriptRoot/../versions/linuxkerneldev"
+
+if ($notCheckedIssues.Count -gt 0) {
     $ret.code = 1
     $ret.lines.Add("microhobby/") | Out-Null
     $ret.lines.Add("linuxkerneldev") | Out-Null
     $ret.lines.Add("-----------------------") | Out-Null
     $ret.lines.Add("OPEN ISSUES: $count") | Out-Null
 
-    foreach ($issue in $obj) {
+    foreach ($issue in $notCheckedIssues) {
         $id = $issue.number
         $title = $issue.title
         $ret.lines.Add("-----------------------") | Out-Null
